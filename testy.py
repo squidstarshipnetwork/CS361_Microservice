e973fda6df8f142dde2c972a2bb43fdba31ebadc
squidstarshipnetwork <72107026+squidstarshipnetwork@users.noreply.github.com>
2022-02-27 02:05:34 -0800

    Improved functionality of scraper, requiring only job title, not URL

--- testy.py
+++ testy.py
@@ -3,25 +3,23 @@ from bs4 import BeautifulSoup
 
 #Stage base of URL
 site = "https://ffxiv.consolegameswiki.com/wiki/"
+#create file pointer to the request file
 
-#Array of all jobs in FFXIV
 jobs = ["Paladin", "Warrior", "Dark Knight", "Gunbreaker",
         "White Mage", "Scholar", "Astrologian", "Sage",
         "Dragoon", "Reaper", "Monk", "Samurai", "Ninja",
         "Bard", "Machinist", "Dancer",
         "Black Mage", "Summoner", "Red Mage", "Blue Mage"]
 
-#grab job title from request file, then throw into job var
 f = open('ws-request.txt', 'r+')
+
+#After reading in the text as a URL address, close the file
 job = f.read()
 
-#replace any spaces with underscores for a valid URL
 job.replace(" ", "_")
 f.close()
 
-#parse to correct string for URL
 URL = site + job
-
 #Grab the page data from the URL with Requests
 page = requests.get(URL)
 
